6f4dc263e47d2e312f9adcc8d08a3c8a08a53993
Michael Bestas <mkbestas@lineageos.org>
2025-05-13 16:02:25 +0000

    avicii: Sync extract scripts with templates

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2023 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -25,6 +24,7 @@ fi
 source "${HELPER}"
 
 # Initialize the helper
+source "${MY_DIR}/../../${VENDOR}/${DEVICE}/setup-makefiles.sh"
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 
 # Warning headers and guards
