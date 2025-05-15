4cc8838305ea264eb8479081b457e72206758ddd
Michael Bestas <mkbestas@lineageos.org>
2025-05-15 13:16:16 +0530

    avicii: Copy media_codecs_vendor to media_codecs
    These are the right codecs to use for this device. The vendor codecs
    file isn't picked up without qcom framework changes.

--- device.mk
+++ device.mk
@@ -273,9 +273,8 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
-    $(LOCAL_PATH)/media/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor.xml
+    $(LOCAL_PATH)/media/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml
 
 PRODUCT_PACKAGES += \
     libavservices_minijail \
