e2833c7c32c16b81f0d80082ab8c8cfceb334246
cjh1249131356 <cjh1249131356@gmail.com>
2025-05-15 20:23:47 +0530

    avicii: Update framework oplus

--- framework-oplus/src/android/common/OplusFrameworkFactory.java
+++ framework-oplus/src/android/common/OplusFrameworkFactory.java
@@ -1,13 +1,23 @@
 package android.common;
 
-public class OplusFrameworkFactory {
-
-    private static OplusFrameworkFactory sOplusFrameworkFactory = null;
+public class OplusFrameworkFactory implements IOplusCommonFactory {
+    public static OplusFrameworkFactory sInstance = null;
 
     public static OplusFrameworkFactory getInstance() {
-        if (sOplusFrameworkFactory == null) {
-            sOplusFrameworkFactory = new OplusFrameworkFactory();
+        if (sInstance == null) {
+            sInstance = new OplusFrameworkFactory();
         }
-        return sOplusFrameworkFactory;
+        return sInstance;
+    }
+
+    @Override
+    public boolean isValid(int index) {
+        boolean validOplus =
+                index < OplusFeatureList.OplusIndex.EndOplusFrameworkFactory.ordinal() &&
+                index > OplusFeatureList.OplusIndex.StartOplusFrameworkFactory.ordinal();
+        boolean vaildOplusOs =
+                index < OplusFeatureList.OplusIndex.EndOplusOsFrameworkFactory.ordinal() &&
+                index > OplusFeatureList.OplusIndex.StartOplusOsFrameworkFactory.ordinal();
+        return vaildOplusOs || validOplus;
     }
 }
--- framework-oplus/src/com/oplus/content/OplusFeatureConfigManager.java
+++ framework-oplus/src/com/oplus/content/OplusFeatureConfigManager.java
@@ -1,5 +1,9 @@
 package com.oplus.content;
 
+import android.text.TextUtils;
+
+import java.util.List;
+
 public class OplusFeatureConfigManager {
 
     public static OplusFeatureConfigManager sOplusFeatureConfigManager = null;
@@ -13,5 +17,9 @@ public class OplusFeatureConfigManager {
 
     public boolean hasFeature(String name) {
         return false;
+    }
+
+     public interface OnFeatureObserver {
+        default void onFeatureUpdate(List<String> features) {}
     }
 }
