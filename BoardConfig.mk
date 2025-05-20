9be496bcb219b3691327fcad2048986a88cc2b69
sreeshankark <sreeshankar0910@gmail.com>
2025-05-20 19:48:10 +0530

    avicii: Switch to NS Kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
-
-# Copyright (C) 2018 The LineageOS Project
+#
+# Copyright (C) 2018-2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -110,8 +110,8 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := BRAND_SHOW_FLAG=oneplus
 TARGET_KERNEL_SOURCE := kernel/oneplus/avicii
-TARGET_KERNEL_CONFIG := vendor/lito-perf_defconfig vendor/debugfs.config
-TARGET_KERNEL_CLANG_VERSION := r487747c
+TARGET_KERNEL_CONFIG := avicii_defconfig debugfs.config
+TARGET_KERNEL_CLANG_VERSION := r547379
 TARGET_KERNEL_CLANG_PATH := $(abspath .)/prebuilts/clang/host/linux-x86/clang-$(TARGET_KERNEL_CLANG_VERSION)
 
 # Platform
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# Copyright (C) 2024 Android Open Source Project
-# Copyright (C) 2024 Sreeshankar K
+# Copyright (C) 2024-2025 Android Open Source Project
+# Copyright (C) 2024-2025 Sreeshankar K
 
 # Color code variables
 R="\033[1;31m";
@@ -11,10 +11,13 @@ N="\033[0m"; # No Color
 
 # Environment variables
 SRC_DIR="${PWD}";
+CLANG_VERSION="r547379";
+CLANG_DIR="${SRC_DIR}/prebuilts/clang/host/linux-x86/clang-${CLANG_VERSION}";
 KERNEL_DIR="${SRC_DIR}/kernel/oneplus/avicii";
 VENDOR_DIR="${SRC_DIR}/vendor/oneplus/avicii";
 APPS_DIR="${SRC_DIR}/vendor/oneplus/apps";
-KERNEL_REPO="https://github.com/sreeshankark/android_kernel_oneplus_sm7250";
+CLANG_TAR="https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-${CLANG_VERSION}.tar.gz";
+KERNEL_REPO="https://github.com/sreeshankark/android_kernel_oneplus_avicii";
 VENDOR_REPO="https://bitbucket.org/sreeshankark/android_vendor_oneplus_avicii";
 APPS_REPO="https://bitbucket.org/sreeshankark/android_vendor_oneplus_apps";
 
@@ -50,10 +53,26 @@ do
 	else
 		echo -e "${R}${NAME} not found${N}";
 		echo -e "${B}Cloning ${NAME}...${N}";
-		bash -c "git clone ${REPO} --depth=1 ${DIR}";
+		bash -c "git clone ${REPO} --depth=1 --recursive ${DIR}";
 		echo -e "${G}Sucessfully cloned ${NAME}${N}";
 	fi
 done;
 }
 
+function chk_clang() {
+if [ -d ${CLANG_DIR} ];
+then
+	echo -e "${G}Clang/LLVM Prebuilts found${N}";
+else
+	echo -e "${R}Clang/LLVM Prebuilts not found${N}";
+	echo -e "${B}Cloning Clang/LLVM Prebuilts...${N}";
+	bash -c "curl -sL ${CLANG_TAR} > clang.tar.gz";
+	bash -c "mkdir ${CLANG_DIR}";
+	bash -c "tar -xvf clang.tar.gz -C ${CLANG_DIR}";
+	bash -c "rm clang.tar.gz";
+	echo -e "${G}Sucessfully cloned Clang/LLVM Prebuilts${N}";
+fi
+}
+
 chk_dependencies;
+chk_clang;
